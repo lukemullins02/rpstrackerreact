4ecf4c0eb1717fc0f77e78fa9bdf672a5b3801aa
= <lukejmullins@gmail.com>
2026-04-27 14:36:11 -0400

    Add dropdownlist

--- src/modules/backlog/components/add-item-modal/add-item-modal.tsx
+++ src/modules/backlog/components/add-item-modal/add-item-modal.tsx
@@ -4,89 +4,108 @@ import { ItemType } from "../../../../core/constants";
 import { EMPTY_STRING } from "../../../../core/helpers";
 import { PtItem } from "../../../../core/models/domain";
 import { PtNewItem } from "../../../../shared/models/dto/pt-new-item";
+import { Input, TextArea } from "@progress/kendo-react-inputs";
+import { DropDownList } from "@progress/kendo-react-dropdowns";
 
 export type AddItemModalProps = {
-    modalShowing: boolean;
-    onNewItemSave: (newItem: PtNewItem) => Promise<PtItem | undefined>;
-    setIsAddModalShowing: React.Dispatch<React.SetStateAction<boolean>>;
+  modalShowing: boolean;
+  onNewItemSave: (newItem: PtNewItem) => Promise<PtItem | undefined>;
+  setIsAddModalShowing: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const initModalNewItem = (): PtNewItem =>  {
-    return {
-        title: EMPTY_STRING,
-        description: EMPTY_STRING,
-        typeStr: 'PBI'
-    };
-}
+const initModalNewItem = (): PtNewItem => {
+  return {
+    title: EMPTY_STRING,
+    description: EMPTY_STRING,
+    typeStr: "PBI",
+  };
+};
 
 export function AddItemModal(props: AddItemModalProps) {
+  const [newItem, setNewItem] = useState(initModalNewItem());
 
-    const [newItem, setNewItem] = useState(initModalNewItem());
-    
-    const modalShowing = props.modalShowing;
-    const setShowModal = props.setIsAddModalShowing;
-    const itemTypesProvider = ItemType.List.map((t) => t.PtItemType);
+  const modalShowing = props.modalShowing;
+  const setShowModal = props.setIsAddModalShowing;
+  const itemTypesProvider = ItemType.List.map((t) => t.PtItemType);
 
-    function onFieldChange(e: any, formFieldName: string) {
-        if (!newItem) {
-            return;
-        }
-        setNewItem({ ...newItem, [formFieldName]: e.target.value });
+  console.log(itemTypesProvider);
+
+  function onFieldChange(e: any, formFieldName: string) {
+    if (!newItem) {
+      return;
     }
+    setNewItem({ ...newItem, [formFieldName]: e.target.value });
+  }
 
-    async function onAddSave() {
-        const createdItem = await props.onNewItemSave(newItem);
-        setShowModal(false);
-        setNewItem(initModalNewItem());
-    }
+  async function onAddSave() {
+    const createdItem = await props.onNewItemSave(newItem);
+    setShowModal(false);
+    setNewItem(initModalNewItem());
+  }
 
-    return (
-        <Modal isOpen={modalShowing}>
-            <div className="modal-header">
-                <h4 className="modal-title" id="modal-basic-title">Add New Item</h4>
-                <button type="button" className="close" onClick={()=>setShowModal(false)} aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+  return (
+    <Modal isOpen={modalShowing}>
+      <div className="modal-header">
+        <h4 className="modal-title" id="modal-basic-title">
+          Add New Item
+        </h4>
+        <button
+          type="button"
+          className="close"
+          onClick={() => setShowModal(false)}
+          aria-label="Close"
+        >
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <ModalBody>
+        <form>
+          <div className="form-group row">
+            <label className="col-sm-2 col-form-label">Title</label>
+            <div className="col-sm-10">
+              <Input
+                defaultValue={newItem.title}
+                onChange={(e) => onFieldChange(e, "title")}
+                name="title"
+                placeholder="Enter title..."
+              />
             </div>
-            <ModalBody>
-                <form>
-                    <div className="form-group row">
-                        <label className="col-sm-2 col-form-label">Title</label>
-                        <div className="col-sm-10">
-                            <input className="form-control" defaultValue={newItem.title} onChange={(e) => onFieldChange(e, 'title')} name="title" />
-                        </div>
-                    </div>
+          </div>
 
-                    <div className="form-group row">
-                        <label className="col-sm-2 col-form-label">Description</label>
-                        <div className="col-sm-10">
-                            <textarea className="form-control" defaultValue={newItem.description} onChange={(e) => onFieldChange(e, 'description')} name="description"></textarea>
-                        </div>
-                    </div>
+          <div className="form-group row">
+            <label className="col-sm-2 col-form-label">Description</label>
+            <div className="col-sm-10">
+              <TextArea
+                className="form-control"
+                defaultValue={newItem.description}
+                onChange={(e) => onFieldChange(e, "description")}
+                name="description"
+                placeholder="Enter description..."
+              ></TextArea>
+            </div>
+          </div>
 
-                    <div className="form-group row">
-                        <label className="col-sm-2 col-form-label">Item Type</label>
-                        <div className="col-sm-10">
-                            <select className="form-control" defaultValue={newItem.typeStr} onChange={(e) => onFieldChange(e, 'typeStr')} name="itemType">
-                                {
-                                    itemTypesProvider.map(t => {
-                                        return (
-                                            <option key={t} value={t}>
-                                                {t}
-                                            </option>
-                                        )
-                                    })
-                                }
-                            </select>
-                        </div>
-                    </div>
-
-                </form >
-            </ModalBody >
-            <ModalFooter>
-                <Button color="secondary" onClick={()=>setShowModal(false)}>Cancel</Button>
-                <Button color="primary" onClick={onAddSave}>Save</Button>{' '}
-            </ModalFooter>
-        </Modal >
-    );
+          <div className="form-group row">
+            <label className="col-sm-2 col-form-label">Item Type</label>
+            <div className="col-sm-10">
+              <DropDownList
+                defaultValue={newItem.typeStr}
+                data={itemTypesProvider}
+                name="typeStr"
+                onChange={(e) => onFieldChange(e, "typeStr")}
+              />
+            </div>
+          </div>
+        </form>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="secondary" onClick={() => setShowModal(false)}>
+          Cancel
+        </Button>
+        <Button color="primary" onClick={onAddSave}>
+          Save
+        </Button>{" "}
+      </ModalFooter>
+    </Modal>
+  );
 }
--- src/modules/backlog/components/item-details/pt-item-details.tsx
+++ src/modules/backlog/components/item-details/pt-item-details.tsx
@@ -1,186 +1,224 @@
 import React, { useEffect, useState } from "react";
 import { PtItem, PtUser } from "../../../../core/models/domain";
-import { PtItemDetailsEditFormModel, ptItemToFormModel } from "../../../../shared/models/forms/pt-item-details-edit-form";
-import { ItemType, PT_ITEM_STATUSES, PT_ITEM_PRIORITIES } from "../../../../core/constants";
+import {
+  PtItemDetailsEditFormModel,
+  ptItemToFormModel,
+} from "../../../../shared/models/forms/pt-item-details-edit-form";
+import {
+  ItemType,
+  PT_ITEM_STATUSES,
+  PT_ITEM_PRIORITIES,
+} from "../../../../core/constants";
 import { Observable } from "rxjs";
 import { AssigneeListModal } from "../assignee-list-modal/assignee-list-modal";
+import { DropDownList } from "@progress/kendo-react-dropdowns";
 
 interface PtItemDetailsComponentProps {
-    item: PtItem;
-    itemSaved: (item: PtItem) => void;
-    usersRequested: () => void;
-    users$: Observable<PtUser[]>;
+  item: PtItem;
+  itemSaved: (item: PtItem) => void;
+  usersRequested: () => void;
+  users$: Observable<PtUser[]>;
 }
 
 export function PtItemDetailsComponent(props: PtItemDetailsComponentProps) {
+  const statusesProvider = PT_ITEM_STATUSES;
+  const prioritiesProvider = PT_ITEM_PRIORITIES;
+  const itemTypesProvider = ItemType.List.map((t) => t.PtItemType);
 
-    const statusesProvider = PT_ITEM_STATUSES;
-    const prioritiesProvider = PT_ITEM_PRIORITIES;
-    const itemTypesProvider = ItemType.List.map((t) => t.PtItemType);
+  const [itemForm, setItemForm] = useState(ptItemToFormModel(props.item));
+  const [users, setUsers] = useState<PtUser[]>([]);
+  const [modalIsShowing, setModalIsShowing] = useState(false);
+  const [selectedAssignee, setSelectedAssignee] = useState<PtUser>(
+    props.item.assignee,
+  );
+  useEffect(() => {
+    notifyUpdateItem();
+  }, [selectedAssignee]);
 
-    const [itemForm, setItemForm] = useState(ptItemToFormModel(props.item));
-    const [users, setUsers] = useState<PtUser[]>([]);
-    const [modalIsShowing, setModalIsShowing] = useState(false);
-    const [selectedAssignee, setSelectedAssignee] = useState<PtUser>(props.item.assignee);
-    useEffect(()=>{
-        notifyUpdateItem();
-    }, [selectedAssignee]);
-
-    
-    function onFieldChange(e: any, formFieldName: string) {
-        if (!itemForm) {
-            return;
-        }
-        (itemForm as any)[formFieldName] = e.target.value;
-    }
-
-    function onNonTextFieldChange(e: any, formFieldName: string) {
-        onFieldChange(e, formFieldName);
-        notifyUpdateItem();
-    }
-
-    function onBlurTextField() {
-        notifyUpdateItem();
-    }
-
-    function notifyUpdateItem() {
-        if (!itemForm) {
-            return;
-        }
-        const updatedItem = getUpdatedItem(props.item, itemForm, selectedAssignee!);
-        props.itemSaved(updatedItem);
-    }
-
-    function getUpdatedItem(item: PtItem, itemForm: PtItemDetailsEditFormModel, assignee: PtUser): PtItem {
-        const updatedItem = Object.assign({}, item, {
-            title: itemForm.title,
-            description: itemForm.description,
-            type: itemForm.typeStr,
-            status: itemForm.statusStr,
-            priority: itemForm.priorityStr,
-            estimate: itemForm.estimate,
-            assignee: assignee
-        });
-        return updatedItem;
-    }
-
-    function assigneePickerOpen() {
-        props.users$.subscribe((users: PtUser[]) => {
-            if (users.length > 0) {
-                setUsers(users);
-                setModalIsShowing(true);
-            }
-        });
-
-        props.usersRequested();
-    }
-
-    function selectAssignee(u: PtUser) {
-        setSelectedAssignee(u);
-        setItemForm({ ...itemForm, assigneeName: u.fullName });
-        setModalIsShowing(false);
-        notifyUpdateItem();
-    }
-
+  function onFieldChange(e: any, formFieldName: string) {
     if (!itemForm) {
-        return null;
+      return;
     }
+    (itemForm as any)[formFieldName] = e.target.value;
+  }
 
-    return (
-        <React.Fragment>
-            <form>
-                <div className="form-group row">
-                    <label className="col-sm-2 col-form-label">Title</label>
-                    <div className="col-sm-10">
-                        <input className="form-control" defaultValue={itemForm.title} onBlur={() => onBlurTextField()} onChange={(e) => onFieldChange(e, 'title')} name="title" />
-                    </div>
-                </div>
+  function onNonTextFieldChange(e: any, formFieldName: string) {
+    onFieldChange(e, formFieldName);
+    notifyUpdateItem();
+  }
 
-                <div className="form-group row">
-                    <label className="col-sm-2 col-form-label">Description</label>
-                    <div className="col-sm-10">
-                        <textarea className="form-control" defaultValue={itemForm.description} onBlur={() => onBlurTextField()} onChange={(e) => onFieldChange(e, 'description')} name="description"></textarea>
-                    </div>
-                </div>
+  function onBlurTextField() {
+    notifyUpdateItem();
+  }
 
-                <div className="form-group row">
-                    <label className="col-sm-2 col-form-label">Item Type</label>
-                    <div className="col-sm-10">
-                        <select className="form-control" defaultValue={itemForm.typeStr} onChange={(e) => onNonTextFieldChange(e, 'typeStr')} name="itemType">
-                            {
-                                itemTypesProvider.map(t => {
-                                    return (
-                                        <option key={t} value={t}>
-                                            {t}
-                                        </option>
-                                    )
-                                })
-                            }
-                        </select>
-                    </div>
-                </div>
+  function notifyUpdateItem() {
+    if (!itemForm) {
+      return;
+    }
+    const updatedItem = getUpdatedItem(props.item, itemForm, selectedAssignee!);
+    props.itemSaved(updatedItem);
+  }
 
-                <div className="form-group row">
-                    <label className="col-sm-2 col-form-label">Status</label>
-                    <div className="col-sm-10">
-                        <select className="form-control" defaultValue={itemForm.statusStr} onChange={(e) => onNonTextFieldChange(e, 'statusStr')} name="status">
-                            {
-                                statusesProvider.map(t => {
-                                    return (
-                                        <option key={t} value={t}>
-                                            {t}
-                                        </option>
-                                    )
-                                })
-                            }
-                        </select>
-                    </div>
-                </div>
+  function getUpdatedItem(
+    item: PtItem,
+    itemForm: PtItemDetailsEditFormModel,
+    assignee: PtUser,
+  ): PtItem {
+    const updatedItem = Object.assign({}, item, {
+      title: itemForm.title,
+      description: itemForm.description,
+      type: itemForm.typeStr,
+      status: itemForm.statusStr,
+      priority: itemForm.priorityStr,
+      estimate: itemForm.estimate,
+      assignee: assignee,
+    });
+    return updatedItem;
+  }
 
-                <div className="form-group row">
-                    <label className="col-sm-2 col-form-label">Estimate</label>
-                    <div className="col-sm-10">
-                        <input className="form-control" type="range" step="1" min="0" max="20" value={itemForm.estimate} onChange={(e) => onNonTextFieldChange(e, 'estimate')} name="estimate" style={{ width: 300 }} />
-                    </div>
-                </div>
+  function assigneePickerOpen() {
+    props.users$.subscribe((users: PtUser[]) => {
+      if (users.length > 0) {
+        setUsers(users);
+        setModalIsShowing(true);
+      }
+    });
 
-                <div className="form-group row">
-                    <label className="col-sm-2 col-form-label">Priority</label>
-                    <div className="col-sm-10">
-                        <select className="form-control" defaultValue={itemForm.priorityStr} onChange={(e) => onNonTextFieldChange(e, 'priorityStr')} name="priority">
-                            {
-                                prioritiesProvider.map(t => {
-                                    return (
-                                        <option key={t} value={t}>
-                                            {t}
-                                        </option>
-                                    )
-                                })
-                            }
-                        </select>
-                    </div>
-                </div>
+    props.usersRequested();
+  }
 
-                <div className="form-group row">
-                    <label className="col-sm-2 col-form-label">Assignee</label>
+  function selectAssignee(u: PtUser) {
+    setSelectedAssignee(u);
+    setItemForm({ ...itemForm, assigneeName: u.fullName });
+    setModalIsShowing(false);
+    notifyUpdateItem();
+  }
 
-                    <div className="col-sm-10">
-                        <img src={selectedAssignee!.avatar} className="li-avatar rounded" />
-                        <span>{itemForm.assigneeName}</span>
+  if (!itemForm) {
+    return null;
+  }
 
-                        <button type="button" className="btn btn-sm btn-outline-secondary" onClick={() => assigneePickerOpen()}>Pick assignee</button>
-                    </div>
-                </div>
-            </form>
+  return (
+    <React.Fragment>
+      <form>
+        <div className="form-group row">
+          <label className="col-sm-2 col-form-label">Title</label>
+          <div className="col-sm-10">
+            <input
+              className="form-control"
+              defaultValue={itemForm.title}
+              onBlur={() => onBlurTextField()}
+              onChange={(e) => onFieldChange(e, "title")}
+              name="title"
+            />
+          </div>
+        </div>
 
-            <AssigneeListModal 
-                users={users} 
-                modalIsShowing={modalIsShowing} 
-                setModalIsShowing={setModalIsShowing} 
-                selectAssignee={selectAssignee} />
+        <div className="form-group row">
+          <label className="col-sm-2 col-form-label">Description</label>
+          <div className="col-sm-10">
+            <textarea
+              className="form-control"
+              defaultValue={itemForm.description}
+              onBlur={() => onBlurTextField()}
+              onChange={(e) => onFieldChange(e, "description")}
+              name="description"
+            ></textarea>
+          </div>
+        </div>
 
+        <div className="form-group row">
+          <label className="col-sm-2 col-form-label">Item Type</label>
+          <div className="col-sm-10">
+            <select
+              className="form-control"
+              defaultValue={itemForm.typeStr}
+              onChange={(e) => onNonTextFieldChange(e, "typeStr")}
+              name="itemType"
+            >
+              {itemTypesProvider.map((t) => {
+                return (
+                  <option key={t} value={t}>
+                    {t}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+        </div>
 
-        </React.Fragment>
-    );
-   
+        <div className="form-group row">
+          <label className="col-sm-2 col-form-label">Status</label>
+          <div className="col-sm-10">
+            <DropDownList
+              data={statusesProvider}
+              defaultValue={itemForm.statusStr}
+              onChange={(e) => onNonTextFieldChange(e, "statusStr")}
+            ></DropDownList>
+          </div>
+        </div>
+
+        <div className="form-group row">
+          <label className="col-sm-2 col-form-label">Estimate</label>
+          <div className="col-sm-10">
+            <input
+              className="form-control"
+              type="range"
+              step="1"
+              min="0"
+              max="20"
+              value={itemForm.estimate}
+              onChange={(e) => onNonTextFieldChange(e, "estimate")}
+              name="estimate"
+              style={{ width: 300 }}
+            />
+          </div>
+        </div>
+
+        <div className="form-group row">
+          <label className="col-sm-2 col-form-label">Priority</label>
+          <div className="col-sm-10">
+            <select
+              className="form-control"
+              defaultValue={itemForm.priorityStr}
+              onChange={(e) => onNonTextFieldChange(e, "priorityStr")}
+              name="priority"
+            >
+              {prioritiesProvider.map((t) => {
+                return (
+                  <option key={t} value={t}>
+                    {t}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+        </div>
+
+        <div className="form-group row">
+          <label className="col-sm-2 col-form-label">Assignee</label>
+
+          <div className="col-sm-10">
+            <img src={selectedAssignee!.avatar} className="li-avatar rounded" />
+            <span>{itemForm.assigneeName}</span>
+
+            <button
+              type="button"
+              className="btn btn-sm btn-outline-secondary"
+              onClick={() => assigneePickerOpen()}
+            >
+              Pick assignee
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <AssigneeListModal
+        users={users}
+        modalIsShowing={modalIsShowing}
+        setModalIsShowing={setModalIsShowing}
+        selectAssignee={selectAssignee}
+      />
+    </React.Fragment>
+  );
 }
